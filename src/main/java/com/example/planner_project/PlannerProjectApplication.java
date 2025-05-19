75e2bdcbb1e39dec15426d791b20afd2b106b21c
xodbs126 <dltvmsskd@naver.com>
2025-05-19 23:31:24 +0900

    05/19 - 프로젝트 생성

---
+++ src/main/java/com/example/planner_project/PlannerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.planner_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/planner_project/PlannerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planner_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
