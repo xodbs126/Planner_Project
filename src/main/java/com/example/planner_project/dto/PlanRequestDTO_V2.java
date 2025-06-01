adc04c791f383b5cee312d41f578b1b63c859e40
xodbs126 <dltvmsskd@naver.com>
2025-06-01 15:18:14 +0900

    05/20 - DB 구조 및 엔티티 설정, 레포지토리, 서비스 설계 05/21 - 필수 기능 구현 완료 05/25 - User 객체 추가, Validation, ExceptionHandler 추가

--- src/main/java/com/example/planner_project/dto/PlanRequestDTO_V2.java
+++ src/main/java/com/example/planner_project/dto/PlanRequestDTO_V2.java
@@ -1,9 +1,14 @@
 package com.example.planner_project.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class PlanRequestDTO_V2 {
+
+    @NotBlank
+    @Size(max = 200)
     private String content;
     private Long userId;
 }
--- src/main/java/com/example/planner_project/dto/PlanUpdateDtoV2.java
+++ src/main/java/com/example/planner_project/dto/PlanUpdateDtoV2.java
@@ -1,12 +1,19 @@
 package com.example.planner_project.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class PlanUpdateDtoV2 {
+
+    @NotBlank
+    @Size(max = 200)
     private String content;
     private Long userId;
     private String userName;
+
+    @NotBlank
     private String password;
 
 }
